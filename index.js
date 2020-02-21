e85326a583ddfdd05e451f6ac797840851fc04e4
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-02-21 17:51:35 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+const PCancelable = require('p-cancelable');
+
+const action = PCancelable.fn(async ({convert, exportOptions, inputPath, outputPath}, onCancel) => {
+	const process = convert([
+		'-i',
+		inputPath,
+		'-vf', 'reverse',
+		...(
+			exportOptions.isMuted ? [] : [
+				'-af', 'reverse'
+			]
+		),
+		outputPath
+	], 'Reversing');
+
+	onCancel(() => {
+		process.cancel();
+	});
+
+	return process;
+});
+
+const reverse = {
+	title: 'Reverse',
+	action
+};
+
+exports.editServices = [reverse];
---
+++ readme.md
@@ -0,0 +1,13 @@
+# kap-reverse [![Build Status](https://travis-ci.org/karaggeorge/kap-reverse.svg?branch=master)](https://travis-ci.org/karaggeorge/kap-reverse)
+
+> [Kap](https://github.com/wulkano/kap) plugin - Reverse Kap recordings
+
+
+## Install
+
+In the `Kap` menu, go to `Preferences…`, select the `Plugins` pane, find this plugin, and toggle it.
+
+
+## Usage
+
+In the editor, click the `+` button, select `Reverse`, configure and export.
